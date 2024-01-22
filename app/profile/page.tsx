c66ad297adfb467c6c835f9786f93094497732bf
daikidomon <daiki.domon@daikidomon.com>
2024-01-22 14:29:44 +0900

    Initial commit

---
+++ app/profile/page.tsx
@@ -0,0 +1,79 @@
+
+"use client"
+import DateFormatter from "@/components/date";
+import { Database } from "@/types/supabasetype";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useEffect, useState } from "react";
+
+/**
+ * ログイン後のマイページ
+ */
+const MyPage = () => {
+  const supabase = createClientComponentClient();
+  const [info, setInfo] = useState<Database["public"]["Tables"]["memberinfo"]["Row"][]>([])
+  useEffect(() => {
+    async function getData() {
+      const { data: info, error } = await supabase.from("memberinfo").select("*");
+      if (error) {
+        throw error
+      }
+      const infoList = []
+      for (let index = 0; index < info.length; index++) {
+        infoList.push(info[index])
+      }
+      setInfo(infoList)
+    }
+    getData();
+  }, []);
+
+  return (
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 pt-20 text-center lg:pt-32">
+      <h1 className="text-2xl font-bold">
+        ユーザ情報
+      </h1>
+      <ul className="m-auto max-w-sm pt-10 space-y-1">
+        {info.map((item, index) => (
+          <li className="text-left" key={item.id}>
+            <dl className="pt-2 border-b-2">
+              <dt className="text-gray-600">
+                ユーザ名
+              </dt>
+              <dd>
+                {item.user_name}
+              </dd>
+            </dl>
+            <dl className="pt-2 border-b-2">
+              <dt className="text-gray-600">
+                誕生日
+              </dt>
+              <dd>
+                <DateFormatter timestamp={item.user_birthday!}></DateFormatter>
+              </dd>
+            </dl>
+            <dl className="pt-2 border-b-2">
+              <dt className="text-gray-600">
+                趣味
+              </dt>
+              <dd>
+                {item.user_hobby}
+              </dd>
+            </dl>
+          </li>
+        ))}
+      </ul>
+      <div className="pt-10">
+        <form action="/auth/logout" method="post">
+          <button
+            className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            type="submit"
+          >
+            ログアウト
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+
+export default MyPage;
---
+++ types/supabasetype.ts
@@ -0,0 +1,189 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      memberinfo: {
+        Row: {
+          id: number
+          user_birthday: string | null
+          user_hobby: string | null
+          user_id: string | null
+          user_name: string | null
+        }
+        Insert: {
+          id?: number
+          user_birthday?: string | null
+          user_hobby?: string | null
+          user_id?: string | null
+          user_name?: string | null
+        }
+        Update: {
+          id?: number
+          user_birthday?: string | null
+          user_hobby?: string | null
+          user_id?: string | null
+          user_name?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "memberinfo_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      members: {
+        Row: {
+          id: number
+          team_id: number | null
+          user_id: string | null
+          user_name: string | null
+        }
+        Insert: {
+          id?: number
+          team_id?: number | null
+          user_id?: string | null
+          user_name?: string | null
+        }
+        Update: {
+          id?: number
+          team_id?: number | null
+          user_id?: string | null
+          user_name?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "members_team_id_fkey"
+            columns: ["team_id"]
+            isOneToOne: false
+            referencedRelation: "teams"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "members_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      teams: {
+        Row: {
+          id: number
+          name: string | null
+        }
+        Insert: {
+          id?: number
+          name?: string | null
+        }
+        Update: {
+          id?: number
+          name?: string | null
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
+      Database["public"]["Views"])
+  ? (Database["public"]["Tables"] &
+      Database["public"]["Views"])[PublicTableNameOrOptions] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof Database["public"]["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
+  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
+  : never
